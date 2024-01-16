0a034feae37f9db7dce068d6ed8986d95f2644a4
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-01-16 11:57:31 +0100

    Implémentation du main de FizzBuzz

--- src/main/java/fr/umontpellier/iut/exercice1/App.java
+++ src/main/java/fr/umontpellier/iut/exercice1/App.java
@@ -3,7 +3,16 @@ package fr.umontpellier.iut.exercice1;
 public class App {
 
     public static void main(String[] args) {
-        throw new RuntimeException("La fonction n'est pas encore implémentée !");
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.getValue(5));
+        System.out.println(fizzBuzz.getValue(3));
+        System.out.println(fizzBuzz.getValue(8));
+        System.out.println("-".repeat(20));
+
+        String[] liste = fizzBuzz.computeList(18);
+        System.out.println("Pour n = 18:");
+        for(String val : liste)
+            System.out.println("  => " + val);
     }
 
 }
