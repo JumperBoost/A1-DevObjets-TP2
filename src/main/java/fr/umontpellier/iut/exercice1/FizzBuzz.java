5a6d8c832f8cc0717a7f0bb97a2e924ebc89e3dc
Franceus-Cointrel Milwenn <milwenn.franceus-cointrel@etu.umontpellier.fr>
2024-01-16 12:21:38 +0100

    Implémentation de getValue qui retourne le nombre en paramètre

--- src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
+++ src/main/java/fr/umontpellier/iut/exercice1/FizzBuzz.java
@@ -3,21 +3,10 @@ package fr.umontpellier.iut.exercice1;
 public class FizzBuzz {
 
     public String getValue(int i) {
-        String[] liste = computeList(i);
-        return liste[i-1];
+        return String.valueOf(i);
     }
 
     public String[] computeList(int i) {
-        String[] liste = new String[i];
-        for(int y = 0; y < i; y++) {
-            liste[y] = "";
-            if((y+1) % 3 == 0)
-                liste[y] += "Fizz";
-            if((y+1) % 5 == 0)
-                liste[y] += "Buzz";
-            if((y+1) % 3 != 0 && (y+1) % 5 != 0)
-                liste[y] += y+1;
-        }
-        return liste;
+        throw new RuntimeException("La fonction n'est pas encore implémentée !");
     }
 }
